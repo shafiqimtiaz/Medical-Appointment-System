0015f1c685607d8e525c7f3337ad57eb4e2b3873
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-02-22 11:36:42 -0500

    add updated SQL

--- api/db/project_db.sql
+++ api/db/project_db.sql
@@ -1,110 +1,83 @@
-create sequence "Users_userID_seq"
-    as integer;
+CREATE SCHEMA IF NOT EXISTS project;
+SET search_path TO project;
 
-create sequence "Patients_patientID_seq"
-    as integer;
-
-create table if not exists users
+CREATE TABLE Users
 (
-    user_id     integer default nextval('"Users_userID_seq"'::regclass) not null
-        constraint users_pk
-            primary key,
-    email       varchar                                                 not null,
-    hashed_pass varchar                                                 not null,
-    name        varchar                                                 not null,
-    dob         date                                                    not null,
-    address     varchar                                                 not null,
-    phone_nbr   varchar                                                 not null,
-    role        varchar                                                 not null
+  user_id SERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(200) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  role VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (user_id)
 );
 
-alter sequence "Users_userID_seq" owned by users.user_id;
-
-create index if not exists users_email_index
-    on users (email);
-
-create table if not exists patients
+CREATE TABLE Patients 
 (
-    patient_id integer default nextval('"Patients_patientID_seq"'::regclass) not null
-        constraint patients_pk
-            primary key,
-    user_id    integer
-        constraint patients_users_null_fk
-            references users
+  patient_id INT NOT NULL,
+  date_of_birth DATE NOT NULL,
+  health_condition VARCHAR(1000) NOT NULL,
+  PRIMARY KEY (patient_id),
+  FOREIGN KEY (patient_id) REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-alter sequence "Patients_patientID_seq" owned by patients.patient_id;
-
-create table if not exists med_staff
+CREATE TABLE Medical_staff
 (
-    staff_id    serial
-        constraint med_staff_pk
-            primary key,
-    user_id     integer               not null
-        constraint med_staff_users_null_fk
-            references users,
-    license_nbr varchar               not null,
-    is_approved boolean default false not null,
-    role        varchar               not null
+  medical_staff_id INT NOT NULL,
+  license_number BIGINT NOT NULL,
+  active BOOL NOT NULL,
+  type INT NOT NULL,
+  PRIMARY KEY (medical_staff_id),
+  FOREIGN KEY (medical_staff_id) REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-create table if not exists managers
+CREATE TABLE Manager
 (
-    manager_id serial
-        constraint managers_pk
-            primary key,
-    user_id    integer not null
-        constraint unique_user
-            unique
-        constraint managers_users_null_fk
-            references users
+  manager_id INT NOT NULL,
+  PRIMARY KEY (manager_id),
+  FOREIGN KEY (manager_id) REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-create table if not exists appointments
+CREATE TABLE Appointments
 (
-    appointment_id serial
-        constraint appointments_pk
-            primary key,
-    staff_id       integer   not null
-        constraint appointments_med_staff_null_fk
-            references med_staff,
-    patient_id     integer   not null
-        constraint appointments_patients_null_fk
-            references patients,
-    time           timestamp not null
+  appointment_id SERIAL NOT NULL,
+  details VARCHAR(1000) NOT NULL,
+  appointment_date TIMESTAMP NOT NULL,
+  active BOOL NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  created_by VARCHAR(50) NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_by VARCHAR(50) NOT NULL,
+  medical_staff_id INT NOT NULL,
+  patient_id INT NOT NULL,
+  PRIMARY KEY (appointment_id),
+  FOREIGN KEY (medical_staff_id) REFERENCES Medical_staff(medical_staff_id),
+  FOREIGN KEY (patient_id) REFERENCES Patients(patient_id)
 );
 
-create index if not exists appointments_patient_id_index
-    on appointments (patient_id);
-
-create index if not exists appointments_staff_id_index
-    on appointments (staff_id);
-
-create table if not exists assessments
+CREATE TABLE Assessments
 (
-    assessment_id      serial
-        constraint assessments_pk
-            primary key,
-    patient_id         integer                 not null
-        constraint assessments_patients_null_fk
-            references patients,
-    assigned_staff     integer
-        constraint assessments_med_staff_null_fk
-            references med_staff,
-    needs_counseling   boolean   default true  not null,
-    time_of_assessment timestamp default now() not null,
-    is_active          boolean   default true  not null,
-    answer             varchar                 not null,
-    answer2            varchar                 not null,
-    answer3            varchar                 not null,
-    answer4            varchar                 not null,
-    answer5            varchar                 not null,
-    answer6            varchar                 not null,
-    answer7            varchar                 not null,
-    answer8            varchar                 not null,
-    answer9            varchar                 not null
+  assessment_id SERIAL NOT NULL,
+  details VARCHAR(1000) NOT NULL,
+  active BOOL NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  created_by VARCHAR(50) NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_by VARCHAR(50) NOT NULL,
+  patient_id INT NOT NULL,
+  medical_staff_id INT,
+  PRIMARY KEY (assessment_id),
+  FOREIGN KEY (patient_id) REFERENCES Patients(patient_id),
+  FOREIGN KEY (medical_staff_id) REFERENCES Medical_staff(medical_staff_id)
 );
 
-create index if not exists assessments_needs_counseling_index
-    on assessments (needs_counseling desc);
-
+CREATE TABLE Answers
+(
+  answer_id SERIAL NOT NULL,
+  answer VARCHAR(1000) NOT NULL,
+  assessment_id INT NOT NULL,
+  PRIMARY KEY (answer_id),
+  FOREIGN KEY (assessment_id) REFERENCES Assessments(assessment_id)
+);
