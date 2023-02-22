ea1d7d3caf382b92cc422dea11db7eaeafc7d982
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-02-21 23:03:16 -0500

    fix seed script

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,117 +1,109 @@
-// import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from "@prisma/client";
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-// const userData: Prisma.UsersCreateInput[] = [
-//   {
-//     email: 'ron@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Rob',
-//     dob: new Date('1990-01-01'),
-//     address: '123 Hogwarts St',
-//     phone_nbr: '123-456-7890',
-//     role: 'patient',
-//   },
-//   {
-//     email: 'harry@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Harry',
-//     dob: new Date('1990-01-01'),
-//     address: '321 Hogwarts St',
-//     phone_nbr: '321-456-7890',
-//     role: 'counselor',
-//   },
-//   {
-//     email: 'hermione@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Hermione',
-//     dob: new Date('1990-01-01'),
-//     address: '456 Hogwarts St',
-//     phone_nbr: '456-456-7890',
-//     role: 'doctor',
-//   },
-//   {
-//     email: 'snape@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Snape',
-//     dob: new Date('1990-01-01'),
-//     address: '789 Hogwarts St',
-//     phone_nbr: '789-456-7890',
-//     role: 'manager',
-//   },
-//   {
-//     email: 'ginny@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Ginny',
-//     dob: new Date('1992-08-11'),
-//     address: '567 Burrow Way',
-//     phone_nbr: '555-555-5555',
-//     role: 'patient',
-//   },
-//   {
-//     email: 'ronan@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Ronan',
-//     dob: new Date('1985-04-23'),
-//     address: '456 Green Isle',
-//     phone_nbr: '444-444-4444',
-//     role: 'counselor',
-//   },
-//   {
-//     email: 'neville@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Neville',
-//     dob: new Date('1988-07-30'),
-//     address: '13 Longbottom Lane',
-//     phone_nbr: '333-333-3333',
-//     role: 'doctor',
-//   },
-//   {
-//     email: 'mcgonagall@mail.com',
-//     hashed_pass: '123456',
-//     name: 'McGonagall',
-//     dob: new Date('1949-10-04'),
-//     address: '1 Hogwarts Castle',
-//     phone_nbr: '222-222-2222',
-//     role: 'manager',
-//   },
-//   {
-//     email: 'luna@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Luna',
-//     dob: new Date('1981-02-13'),
-//     address: '11 Ravenclaw Tower',
-//     phone_nbr: '111-111-1111',
-//     role: 'patient',
-//   },
-//   {
-//     email: 'hagrid@mail.com',
-//     hashed_pass: '123456',
-//     name: 'Hagrid',
-//     dob: new Date('1928-12-06'),
-//     address: '7 Hagrid Hut',
-//     phone_nbr: '999-999-9999',
-//     role: 'doctor',
-//   }
-// ]
+const userData: Prisma.UsersCreateInput[] = [
+  {
+    name: "Ron",
+    address: "123 Hogwarts St",
+    email: "ron@mail.com",
+    password: "123456",
+    role: "patient",
+    created_at: new Date("1990-01-01"),
+  },
+  {
+    name: "Hermione",
+    address: "456 Grimmauld Pl",
+    email: "hermione@mail.com",
+    password: "abcdef",
+    role: "patient",
+    created_at: new Date("1991-04-05"),
+  },
 
-// async function main() {
-//   console.log(`Start seeding ...`)
-//   for (const u of userData) {
-//     const user = await prisma.users.create({
-//       data: u,
-//     })
-//     console.log(`Created user with id: ${user.user_id}`)
-//   }
-//   console.log(`Seeding finished.`)
-// }
+  {
+    name: "Harry",
+    address: "789 Godric's Hollow",
+    email: "harry@mail.com",
+    password: "ghijkl",
+    role: "patient",
+    created_at: new Date("1992-07-31"),
+  },
 
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+  {
+    name: "Ginny",
+    address: "23 Ottery St Catchpole",
+    email: "ginny@mail.com",
+    password: "mnopqr",
+    role: "patient",
+    created_at: new Date("1993-08-11"),
+  },
+  {
+    name: "Draco",
+    address: "13 Malfoy Manor",
+    email: "draco@mail.com",
+    password: "stuvwx",
+    role: "patient",
+    created_at: new Date("1994-06-05"),
+  },
+  {
+    name: "Albus",
+    address: "222 Hogsmeade",
+    email: "albus@mail.com",
+    password: "yz1234",
+    role: "counselor",
+    created_at: new Date("1995-03-01"),
+  },
+  {
+    name: "Severus",
+    address: "55 Spinner's End",
+    email: "severus@mail.com",
+    password: "567890",
+    role: "manager",
+    created_at: new Date("1996-01-09"),
+  },
+  {
+    name: "Minerva",
+    address: "10 Wimbourne Pl",
+    email: "minerva@mail.com",
+    password: "ab2345",
+    role: "doctor",
+    created_at: new Date("1997-03-04"),
+  },
+  {
+    name: "Remus",
+    address: "12 Grimmauld Pl",
+    email: "remus@mail.com",
+    password: "cd3456",
+    role: "doctor",
+    created_at: new Date("1998-08-10"),
+  },
+  {
+    name: "Sirius",
+    address: "1 Padfoot Lane",
+    email: "sirius@mail.com",
+    password: "ef4567",
+    role: "counselor",
+    created_at: new Date("1999-06-16"),
+  },
+];
+
+async function main() {
+  console.log(`Start seeding ...`);
+  for (const u of userData) {
+    const user = await prisma.users.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.user_id}`);
+  }
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
