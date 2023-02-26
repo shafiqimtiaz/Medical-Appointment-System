7967ea5661089efc22ca01ab4bb8f56d1413f732
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-02-26 16:34:37 -0500

    schema updated

--- api/src/controller/manager/manager.controller.ts
+++ api/src/controller/manager/manager.controller.ts
@@ -14,33 +14,37 @@ managerRouter.get("/newRegistrations", async (req, res) => {
   }
 });
 
-managerRouter.get("/approveRegistration/:id", async (req, res) => {
+managerRouter.put("/approveRegistration/:id", async (req, res) => {
   if (!checkAuth(req)) return res.status(401).send("Unauthorized");
   try {
-    await managerService.approveRegistration(+req.params.id);
-    res.status(200).json("Registration approved");
+    const registration = await managerService.approveRegistration(
+      +req.params.id
+    );
+    res.status(200).json(registration);
   } catch (err) {
     console.error(err);
     res.status(500).send("Failed to approve registration");
   }
 });
 
-managerRouter.get("/denyRegistration/:id", async (req, res) => {
+managerRouter.delete("/denyRegistration/:id", async (req, res) => {
   if (!checkAuth(req)) return res.status(401).send("Unauthorized");
   try {
-    await managerService.rejectRegistration(+req.params.id);
-    res.status(200).status(200).json("Registration denied");
+    const registration = await managerService.rejectRegistration(
+      +req.params.id
+    );
+    res.status(200).status(200).json({ Deleted: registration });
   } catch (err) {
     console.error(err);
     res.status(500).send("Failed to deny registrations");
   }
 });
 
-managerRouter.get("/deletePatient/:id", async (req, res) => {
+managerRouter.delete("/deletePatient/:id", async (req, res) => {
   if (!checkAuth(req)) return res.status(401).send("Unauthorized");
   try {
-    await managerService.deletePatient(+req.params.id);
-    res.status(200).json("Patient deleted");
+    const patient = await managerService.deletePatient(+req.params.id);
+    res.status(200).json({ Deleted: patient });
   } catch (e) {
     console.error(e);
     res.status(500).send("Something went wrong");
