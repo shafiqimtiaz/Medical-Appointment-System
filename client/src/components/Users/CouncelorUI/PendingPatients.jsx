2d1e2b32f01f5d185a0dc03bed11fd5155c3906e
Hadi <hadiahmad4104@gmail.com>
2023-03-17 19:23:44 -0400

    List of all patients with there assesment (approve/deny)

--- client/src/components/Users/CouncelorUI/PendingPatients.jsx
+++ client/src/components/Users/CouncelorUI/PendingPatients.jsx
@@ -1,81 +1,148 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect, useMemo } from 'react';
 import { Table, Button, Modal, List } from 'antd';
 import { QuestionCircleTwoTone } from '@ant-design/icons'
+import { notification } from "antd";
+//import {ParentTable} from "/Users/hadi/Desktop/Concordia/Soen6841/SPM_6841_Project/client/src/components/Admin/ParentTable.jsx"
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-const data = [
-  {
-    "id": 1,
-    "name": "John Smith",
-    "email": "john.smith@example.com",
-    "age": 30,
-    "address": "123 Main St, Anytown USA",
-    "number": "(555) 555-1234"
-  },
-  {
-    "id": 2,
-    "name": "Jane Doe",
-    "email": "jane.doe@example.com",
-    "age": 25,
-    "address": "456 Elm St, Anytown USA",
-    "number": "(555) 555-5678"
-  },
-  {
-    "id": 3,
-    "name": "Bob Johnson",
-    "email": "bob.johnson@example.com",
-    "age": 40,
-    "address": "789 Oak St, Anytown USA",
-    "number": "(555) 555-9012"
-  },
-  {
-    "id": 4,
-    "name": "Linda Garcia",
-    "email": "linda.garcia@example.com",
-    "age": 32,
-    "address": "789 Oak St, Anytown USA",
-    "number": "(555) 555-2468"
-  },
-  {
-    "id": 5,
-    "name": "Michael Chen",
-    "email": "michael.chen@example.com",
-    "age": 41,
-    "address": "222 Maple St, Anytown USA",
-    "number": "(555) 555-3690"
-  },
-  {
-    "id": 6,
-    "name": "Grace Lee",
-    "email": "grace.lee@example.com",
-    "age": 26,
-    "address": "101 Cherry Lane, Anytown USA",
-    "number": "(555) 555-1357"
-  },
-  {
-    "id": 7,
-    "name": "Daniel Park",
-    "email": "daniel.park@example.com",
-    "age": 33,
-    "address": "555 Oak St, Anytown USA",
-    "number": "(555) 555-9012"
-  },
-  {
-    "id": 8,
-    "name": "Michelle Wong",
-    "email": "michelle.wong@example.com",
-    "age": 29,
-    "address": "777 Main St, Anytown USA",
-    "number": "(555) 555-5678"
+
+//Fetch all patients and add them to table
+//Activate button if assement is true 
+
+
+const showError = () => {
+  notification.open({
+    message: "Error !!",
+    placement: "top",
+  });
+};
+
+const showSuccess = () => {
+  notification.open({
+    message: "Success !!",
+    placement: "top",
+  });
+};
+
+function calculateAge(dateOfBirth) {
+  //console.log(dateOfBirth);
+  let [year, month, day] = dateOfBirth.split("-");
+  day = day.substring(0, 2);
+  //console.log(year + " " + month + " " + day);
+  const today = new Date();
+  const birthDate = new Date(year, month - 1, day);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
   }
-]
+  return age;
+}
+
+function getAssessmentID(assessments) {
+  let assessmentID = null;
+  if (assessments.length === 0) {
+    return null;
+  } 
+  else {
+    assessments.forEach((assessment) => {
+      if (assessment.active === true && assessment.medical_staff_id === null) {
+        assessmentID = assessment.assessment_id;
+      }
+      else if(assessment.active === true && assessment.medical_staff_id !== null)
+      {
+        assessmentID =  "Assigned";
+      }
+    });
+  }
+  return assessmentID;
+}
 
 
-export default function PendingPatients() {
 
+export default function PendingPatients({accessToken}) {
+
+  const { currentUser } = useSelector((state) => state.user);
   const [visible, setVisible] = useState(false);
-  //const [data2, setData] = useState(null);
+  const [data, setData] = useState(null);
+  const [selectedId, setSelectedId] = useState(null);
+  const [assessment, setAssessment] = useState([
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Little interest or pleasure in doing things?", answer: "" ,id_ :1},
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Feeling down, depressed or hopless?" , answer: "",id :2 },
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Trouble falling asleep, staying asleep, or sleeping too much?", answer: "",id:3 },
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Feeling tired or having little energy?", answer: "",id:4 },
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Poor appetite or overeating?", answer: "",id:5 },
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Feeling bad about yourself - or that you're a failure or have let yourself or your family down",answer: "",id:6 },
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Trouble concentrating on things, such as reading the newspaper or watching television?",answer: "",id:7 },
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Moving or speaking so slowly that other people could have noticed. Or, the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",answer: "",id:8 },
+    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Thoughts that you would be better off dead or of hurting yourself in some way?",answer: "",id:9 },
+  ]);
 
-  const handleOpenModal = () => {
+
+  const headers = useMemo(
+    () => ({ Authorization: `Bearer ${currentUser.access_token}` }),
+    [currentUser.access_token]
+  );
+
+
+  //Fetching data
+  const mapData = (data) => {
+    const mapped = data.map((item) => {
+      const flag = getAssessmentID(item.assessments);
+      if(flag==="Assigned") //Ignore entry if patient is already assigned
+      {
+        return null;
+      }
+      return {
+        key:item.patient_id,
+        id: item.patient_id,
+        name: item.user.name,
+        email: item.user.email,
+        age: calculateAge(item.user.date_of_birth),
+        address: item.user.address,
+        number: item.user.phone_number,
+        assessments: getAssessmentID(item.assessments),
+      };
+    }).filter(item => item !== null);; // filter out null values
+    setData(mapped);
+  };
+  
+  useEffect(() => {
+    axios
+      .get(`/counselor/patients`, { headers })
+      .then((res) => {
+        //console.log(res.data)
+        mapData(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, [headers,data]);
+  ///////
+
+
+
+  const fetchAnswers = async (selectedId) => {
+    try {
+        const response = await axios.get(`counselor/assessments/${selectedId}`,{ headers });
+        const { answers } = response.data;
+        const updatedAnswers = answers.map(a => a.answer);
+        const updatedAssessment = assessment.map((a, index) => {
+          return { ...a, answer: updatedAnswers[index] };
+        });
+        setAssessment(updatedAssessment);
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to fetch data from API');
+    }
+  };
+
+
+  const handleOpenModal = (selectedId) => {
+    setSelectedId(selectedId)
+    fetchAnswers(selectedId);
     setVisible(true);
   };
 
@@ -83,23 +150,29 @@ export default function PendingPatients() {
     setVisible(false);
   };
 
-  const [assesment, setData] = useState([
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Little interest or pleasure in doing things?", answer: 25 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Feeling down, depressed or hopless?" , answer: 30 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Trouble falling asleep, staying asleep, or sleeping too much?", answer: 40 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Feeling tired or having little energy?", answer: 25 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Poor appetite or overeating?", answer: 25 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Feeling bad about yourself - or that you're a failure or have let yourself or your family down",answer: 25 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Trouble concentrating on things, such as reading the newspaper or watching television?",answer: 25 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Moving or speaking so slowly that other people could have noticed. Or, the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",answer: 25 },
-    { question: "Over the past 2 weeks, how often have you been bothered by any of the following problems: Thoughts that you would be better off dead or of hurting yourself in some way?",answer: 25 },
-  ]);
 
-  // const fetchData = async () => {
-  //   const response = await fetch('https://example.com/api/data');
-  //   const data = await response.json();
-  //   setData(data);
-  // };
+  const handleApprove = async (id) => {
+    try {
+      await axios.put(`/counselor/assessments/approve/${selectedId}`, null,{ headers });
+      showSuccess();
+      setVisible(false);
+    } catch (error) {
+      console.error(error.response);
+      showError();
+      setVisible(false);
+    }
+  };
+  const handleDeny = async (id) => {
+    try {
+      await axios.delete(`counselor/assessments/delete/${selectedId}`, { headers });
+      showSuccess();
+      setVisible(false);
+    } catch (error) {
+      console.error(error.response);
+      showError();
+      setVisible(false);
+    }
+  };
 
   const columns = [
     {
@@ -133,14 +206,18 @@ export default function PendingPatients() {
       key: 'number',
     },
     {
+      key: 'assessments',
+    },
+    {
       title: 'Action',
       key: 'action',
-      render: () => (
+      render: (record) => (
         <>
           <Button
+            disabled={record.assessments!=null ? false : true}
             type="primary"
             onClick={() => {
-              handleOpenModal();
+              handleOpenModal(record.assessments);
             }}
             style={{ borderRadius: '5px' }}
           >
@@ -153,12 +230,23 @@ export default function PendingPatients() {
             onCancel={handleOk}
             mask={false}
             width={1000}
+            footer={[
+              <Button key="back" onClick={handleOk}>
+                Cancel
+              </Button>,
+              <Button key="deny" type="primary" danger onClick={() => handleDeny()} style={{ borderRadius: '5px' }}>
+                Deny
+              </Button>,
+              <Button key="approve" type="primary" onClick={() => handleApprove()} style={{ borderRadius: '5px' }}>
+                Approve
+              </Button>,
+            ]}
           >
             <List
-              dataSource={assesment}
+              dataSource={assessment}
               renderItem={item => (
                 <>
-                  <List.Item>
+                  <List.Item key={item.question}>
                   <div>
                       <QuestionCircleTwoTone />
                       <span>  </span>
@@ -166,7 +254,7 @@ export default function PendingPatients() {
                       <List
                         dataSource={[item.answer]}
                         renderItem={answer => (
-                          <List.Item>
+                          <List.Item key={item.id}>
                             <span style={{fontWeight: 'bold' }} >{answer}</span>
                           </List.Item>
                         )}
