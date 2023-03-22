3f93d3514da0b52aa9a41f13a1ae21b084ad03b6
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-03-22 18:30:18 -0400

    add other user seed info

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -8,14 +8,82 @@ async function main() {
       name: "Manager",
       address: "Montreal",
       email: "m@spm.com",
-      date_of_birth: new Date("1996-02-20 19:36:12.928"),
+      date_of_birth: new Date("1986-02-20 00:00:00.00"),
       phone_number: "514-304-3434",
       password: "123456",
       role: "manager",
     },
   });
 
-  console.log({ manager });
+  const p1 = await prisma.users.upsert({
+    where: { email: "p1@spm.com" },
+    update: {},
+    create: {
+      name: "Patient 1",
+      address: "Montreal",
+      email: "p1@spm.com",
+      date_of_birth: new Date("1991-08-20 00:00:00.00"),
+      phone_number: "514-304-3434",
+      password: "123456",
+      role: "patient",
+    },
+  });
+
+  const p2 = await prisma.users.upsert({
+    where: { email: "p2@spm.com" },
+    update: {},
+    create: {
+      name: "Patient 2",
+      address: "Montreal",
+      email: "p2@spm.com",
+      date_of_birth: new Date("1990-10-10 00:00:00.00"),
+      phone_number: "514-304-3434",
+      password: "123456",
+      role: "patient",
+    },
+  });
+
+  const c1 = await prisma.users.upsert({
+    where: { email: "c1@spm.com" },
+    update: {},
+    create: {
+      name: "Counselor 1",
+      address: "Montreal",
+      email: "c1@spm.com",
+      date_of_birth: new Date("1980-10-12 00:00:00.00"),
+      phone_number: "514-304-3434",
+      password: "123456",
+      role: "medical_staff",
+      medical_staff: {
+        create: {
+          license_number: "9007199254740991",
+          type: "c",
+        },
+      },
+    },
+  });
+
+  const d1 = await prisma.users.upsert({
+    where: { email: "d1@spm.com" },
+    update: {},
+    create: {
+      name: "Doctor 1",
+      address: "Montreal",
+      email: "d1@spm.com",
+      date_of_birth: new Date("1970-10-12 00:00:00.00"),
+      phone_number: "514-304-3434",
+      password: "123456",
+      role: "medical_staff",
+      medical_staff: {
+        create: {
+          license_number: "9007199254740991",
+          type: "d",
+        },
+      },
+    },
+  });
+
+  console.log({ manager, p1, p2, c1, d1 });
 }
 main()
   .then(async () => {
