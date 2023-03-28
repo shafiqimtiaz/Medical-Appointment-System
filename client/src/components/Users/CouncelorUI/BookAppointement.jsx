a65468dfbbe9d0c6486d7fa4f101d53d05b59f6d
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-03-27 21:57:30 -0400

    add duplicate appointment check

--- client/src/components/Users/CouncelorUI/BookAppointement.jsx
+++ client/src/components/Users/CouncelorUI/BookAppointement.jsx
@@ -1,7 +1,7 @@
-import { Button, Form, Select, DatePicker, notification } from 'antd';
-import axios from 'axios';
-import React, { useEffect, useMemo, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { Button, Form, Select, DatePicker, notification } from "antd";
+import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
 const layout = {
   labelCol: {
     span: 8,
@@ -17,11 +17,12 @@ const tailLayout = {
   },
 };
 
-export default function BookAppointement() {
+export default function BookAppointment() {
   const { currentUser } = useSelector((state) => state.user);
-  const [patients,setPatients] = useState([]);
-  const [patient,setPatient] = useState("")
-  const [date,setDate] = useState("");
+  const [patients, setPatients] = useState([]);
+  const [patient, setPatient] = useState("");
+  const [appointments, setAppointments] = useState([]);
+  const [date, setDate] = useState("");
   const [id, setId] = useState(0);
 
   const headers = useMemo(
@@ -29,66 +30,109 @@ export default function BookAppointement() {
     [currentUser.access_token]
   );
 
-  const appointementNotification = () => {
+  const appointmentNotification = () => {
     notification.open({
-      message: "Your appointement has been successfully booked!",
+      message: "Your appointment has been successfully booked!",
       placement: "top",
     });
   };
   const errorNotification = () => {
     notification.open({
-      message: "It seems there was an error when trying to book an appointement",
+      message: "It seems there was an error when trying to book an appointment",
       placement: "top",
     });
   };
 
-
-
-  const handleDateChange = (date) => {
-    setDate(date);
+  const duplicateNotification = () => {
+    notification.open({
+      message: "Sorry, you already have an appointment at this time",
+      placement: "top",
+    });
   };
 
-  const handleAppointement = async() =>{
-    let appointement = null;
-    let editedDate = new Date(date);
-    editedDate.setHours(editedDate.getHours() - 4);
+  const handleDateChange = (date) => {
+    let newDate = new Date(date);
+    newDate.setHours(newDate.getHours() - 4, newDate.getMinutes(), 0, 0);
+    setDate(newDate);
+  };
+
+  const getAppointments = async () => {
+    await axios
+      .get("/counselor/appointment", { headers })
+      .then((response) => response)
+      .then((res) => {
+        setAppointments(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const getPatients = async () => {
+    await axios
+      .get("/counselor/patients", { headers })
+      .then((response) => response)
+      .then((res) => {
+        setPatients(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const hasDuplicateTime = async (appointment) => {
+    for (const element of appointments) {
+      const convDate1 = new Date(element.appointment_date).toISOString();
+      const convDate2 = appointment.appointmentDate.toISOString();
+      if (convDate1 === convDate2) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const createAppointment = async (appointment) => {
+    await axios.post("/counselor/appointment", appointment, {
+      headers,
+    });
+    await getAppointments();
+  };
+
+  const handleAppointment = async () => {
+    await getAppointments();
     try {
-      appointement = {
+      let appointment = {
         patient_Id: parseInt(id.key),
         medicalStaff_Id: currentUser.user_id,
-        appointmentDate: editedDate
+        appointmentDate: date,
+      };
+
+      if (appointments.length === 0) {
+        createAppointment(appointment);
+        appointmentNotification();
+      } else {
+        if (await hasDuplicateTime(appointment)) {
+          duplicateNotification();
+        } else {
+          createAppointment(appointment);
+          appointmentNotification();
+        }
       }
-    const res = await axios.post("/counselor/appointment", appointement, {headers})
-    appointementNotification();
-
+    } catch (error) {
+      errorNotification();
     }
-    catch(error){
-      errorNotification()
-    }
+  };
 
-  }
-
-  useEffect(()=>{
-    axios
-    .get("/counselor/patients", { headers })
-    .then((response) => response)
-    .then((res) => {
-      setPatients(res.data);
-    })
-    .catch((error) => console.log(error));
-
-  },[])
+  useEffect(() => {
+    getPatients();
+    getAppointments();
+  }, []);
 
   const formRef = React.useRef(null);
   return (
-    
     <Form
       {...layout}
       ref={formRef}
       name="control-ref"
       style={{
         maxWidth: 600,
-        marginTop: 50
+        marginTop: 50,
       }}
     >
       <Form.Item
@@ -100,7 +144,14 @@ export default function BookAppointement() {
           },
         ]}
       >
-        <DatePicker showTime onChange={handleDateChange} value={date}/>
+        <DatePicker
+          showTime
+          minuteStep={15}
+          hourStep={1}
+          format="YYYY/MM/DD HH:mm"
+          onChange={handleDateChange}
+          value={date}
+        />
       </Form.Item>
       <Form.Item
         name="patient"
@@ -111,16 +162,24 @@ export default function BookAppointement() {
           },
         ]}
       >
-      <Select onChange={(value,key)=> {setId(key); setPatient(value)}}>
-      {patients.map(patient =>(  
-      <Select.Option key={patient.patient_id} value={patient.user.name} >{patient.user.name}</Select.Option> 
-      ))}     
-      </Select>
+        <Select
+          onChange={(value, key) => {
+            setId(key);
+            setPatient(value);
+          }}
+        >
+          {patients.map((patient) => (
+            <Select.Option key={patient.patient_id} value={patient.user.name}>
+              {patient.user.name}
+            </Select.Option>
+          ))}
+        </Select>
       </Form.Item>
       <Form.Item {...tailLayout}>
-        <Button type="primary" onClick={handleAppointement}>
+        <Button type="primary" onClick={handleAppointment}>
           Book
         </Button>
       </Form.Item>
     </Form>
-  );}
+  );
+}
