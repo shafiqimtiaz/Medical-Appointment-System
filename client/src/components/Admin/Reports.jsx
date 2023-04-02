8dc984116284446a12b144b45a34c8c8d36d766c
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-04-02 16:25:50 -0400

    setup line chart

--- client/src/components/Admin/Reports.jsx
+++ client/src/components/Admin/Reports.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useEffect, useState, useMemo } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,6 +10,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 ChartJS.register(
@@ -23,16 +24,77 @@ ChartJS.register(
 );
 
 export default function Reports() {
-  // const [chart, setChart] = useState({});
+  const [line, setLine] = useState({});
 
-  // useEffect(() => {
-  //   axios
-  //     .get("/manager/assessment/stats")
-  //     .then((response) => setChart(response.data))
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     });
-  // }, []);
+  const location = useLocation();
+
+  const accessToken = location.state.val;
+
+  const headers = useMemo(
+    () => ({ Authorization: `Bearer ${accessToken}}` }),
+    [accessToken]
+  );
+
+  useEffect(() => {
+    axios
+      .get("/manager/assessment/stats", { headers })
+      .then((response) => setLine(response.data))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const dataHour = {
+    labels: [
+      "00",
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+    ],
+    datasets: [
+      {
+        label: "Dataset 1",
+        // data: labels.map(() =>
+        //   faker.datatype.number({ min: -1000, max: 1000 })
+        // ),
+        data: [1, 4, 3, 12, 6, 10],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        // data: labels.map(() =>
+        //   faker.datatype.number({ min: -1000, max: 1000 })
+        // ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  const dataWeek = {};
+
+  const dataMonth = {};
 
   const options = {
     responsive: true,
@@ -47,40 +109,11 @@ export default function Reports() {
     },
   };
 
-  const labels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-  ];
-
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "Dataset 1",
-        // data: labels.map(() =>
-        //   faker.datatype.number({ min: -1000, max: 1000 })
-        // ),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      {
-        label: "Dataset 2",
-        // data: labels.map(() =>
-        //   faker.datatype.number({ min: -1000, max: 1000 })
-        // ),
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  };
   return (
     <div>
-      <Line options={options} data={data} />
+      <Line options={options} data={dataHour} />
+      {/* <Line options={options} data={dataWeek} /> */}
+      {/* <Line options={options} data={dataMonth} /> */}
     </div>
   );
 }
