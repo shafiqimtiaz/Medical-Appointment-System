fe3dd420059e5a1cfff0c90ee28c28412ca79e84
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-06-16 02:19:07 -0400

    add dotenv and axios baseurl

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_URL;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
